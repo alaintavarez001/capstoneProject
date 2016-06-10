d66ab3a6184abab915184e9385f0d8b046f7a40f
alaintavarez001 <alain.tavarez001@gmail.com>
2016-06-09 21:13:13 -0400

    got databse working

---
+++ script.js
@@ -0,0 +1,20 @@
+
+var config = {
+  apiKey: "AIzaSyAFz6meHC_mPZXfNxs_3w3zRYsn444Za-g",
+  authDomain: "capstoneproject-d0813.firebaseapp.com",
+  databaseURL: "https://capstoneproject-d0813.firebaseio.com",
+  storageBucket: "",
+};
+var app= firebase.initializeApp(config);
+
+
+function changeTheme(theme){
+  document.getElementById("themeColors").setAttribute("href", theme);
+  localStorage.setItem('theme', theme);
+}
+
+function loadTheme(){
+  if(localStorage.getItem('theme')){
+    changeTheme(localStorage.getItem('theme'));
+  }
+}
